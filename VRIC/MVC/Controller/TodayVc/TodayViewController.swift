7ee05a590fe4ddf1da6747c523297efcf52c93a7
Karan Mehra <karanmehra.zeroit@gmail.com>
2021-12-16 17:35:36 +0530

    Complete Home Screen.

--- VRIC/MVC/Controller/TodayVc/TodayViewController.swift
+++ VRIC/MVC/Controller/TodayVc/TodayViewController.swift
@@ -8,23 +8,113 @@
 import UIKit
 
 class TodayViewController: UIViewController {
-
+    
     @IBOutlet var BgView: UIView!
+    @IBOutlet weak var BtnNotification: UIButton!
+    @IBOutlet weak var BtnSearch: UIButton!
+    @IBOutlet weak var BtnPrayerTime: UIButton!
+    @IBOutlet weak var BtnArQibla: UIButton!
+    @IBOutlet weak var BtnPrayerRequest: UIButton!
+    @IBOutlet weak var BtnQutranRadio: UIButton!
+    @IBOutlet weak var BtnMore: UIButton!
+    @IBOutlet weak var BtnTrendingVideoMore: UIButton!
+    @IBOutlet weak var BtnLiveBroadcastMore: UIButton!
+    @IBOutlet weak var TimeCollectionVIew: UICollectionView!
+    @IBOutlet weak var EventsCollectionVIew: UICollectionView!
+    @IBOutlet weak var TrendingCollectionVIew: UICollectionView!
+    @IBOutlet weak var TodaySearchbar: UISearchBar!
+    @IBOutlet weak var LiveCollectionVIew: UICollectionView!
+    @IBOutlet weak var PodcastCollectionVIew: UICollectionView!
+    @IBOutlet weak var RamadanCollectionVIew: UICollectionView!
+    @IBOutlet weak var LikeCollectionVIew: UICollectionView!
+    @IBOutlet weak var BtnPodcastMore: UIButton!
+    @IBOutlet weak var BtnDonateNow: UIButton!
+    @IBOutlet weak var BtnJoinNow: UIButton!
+    @IBOutlet weak var BtnSignup: UIButton!
+    @IBOutlet weak var BtnLikeMore: UIButton!
+    @IBOutlet weak var TimePageVIew: UIPageControl!
+    @IBOutlet weak var UpcomingPageVIew: UIPageControl!
+    @IBOutlet weak var RamadanPageView: UIPageControl!
+    @IBOutlet weak var ImgJoinNow: UIImageView!
+    
+    @IBOutlet weak var ImgNewsLetter: UIImageView!
+    
+    // @IBOutlet weak var BtnPodcastMore: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        self.TodaySearchbar.isHidden = true
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func ActionSearchBar(_ sender: Any) {
+        self.TodaySearchbar.isHidden = false
+        
+        
     }
-    */
-
+    @IBAction func ActionNotifications(_ sender: Any) {
+        self.TodaySearchbar.isHidden = true
+        
+    }
+    
 }
+
+extension TodayViewController : UICollectionViewDelegate, UICollectionViewDataSource
+{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+             if   collectionView == TimeCollectionVIew {return 10}
+        else if   collectionView == EventsCollectionVIew {return 10}
+        else if   collectionView == TrendingCollectionVIew {return 10}
+        else if   collectionView == LiveCollectionVIew {return 10}
+        else if   collectionView == PodcastCollectionVIew {return 10}
+        else if   collectionView == RamadanCollectionVIew {return 10}
+        else if   collectionView == LikeCollectionVIew {return 10}
+        return 0}
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        if  collectionView == TimeCollectionVIew {  let TimeCell = collectionView.dequeueReusableCell(withReuseIdentifier: "TimeCollectionVIewCell", for: indexPath) as! TimeCollectionVIewCell
+            return TimeCell}
+        
+        else if   collectionView == EventsCollectionVIew {  let EventsCell = collectionView.dequeueReusableCell(withReuseIdentifier: "EventsCollectionVIewCell", for: indexPath) as! EventsCollectionVIewCell
+            return EventsCell}
+        
+        else if  collectionView == TrendingCollectionVIew {  let TrendingCell = collectionView.dequeueReusableCell(withReuseIdentifier: "TrendingCollectionVIewCell", for: indexPath) as! TrendingCollectionVIewCell
+            
+            return TrendingCell
+            
+        }
+        
+            else if   collectionView == LiveCollectionVIew {  let LiveCell = collectionView.dequeueReusableCell(withReuseIdentifier: "LiveCollectionVIewCell", for: indexPath) as! LiveCollectionVIewCell
+                return LiveCell}
+            
+            else if  collectionView == PodcastCollectionVIew {  let PodcastCell = collectionView.dequeueReusableCell(withReuseIdentifier: "PodcastCollectionVIewCell", for: indexPath) as! PodcastCollectionVIewCell
+                return PodcastCell
+                
+            }
+            
+                else if   collectionView == RamadanCollectionVIew {  let RamadanCell = collectionView.dequeueReusableCell(withReuseIdentifier: "RamadanCollectionVIewCell", for: indexPath) as! RamadanCollectionVIewCell
+                    return RamadanCell}
+                
+                else if  collectionView == LikeCollectionVIew {  let LikeCell = collectionView.dequeueReusableCell(withReuseIdentifier: "LikeCollectionVIewCell", for: indexPath) as! LikeCollectionVIewCell
+                    
+                    return LikeCell
+                    
+                }
+                    
+                    
+                    
+                    
+                    
+                    
+                  
+                
+                return UICollectionViewCell()
+                
+            }
+            
+            
+        }
+
+
+
+
---
+++ VRIC/MVC/View/TodayVcCells/TimeCollectionVIewCell.swift
@@ -0,0 +1,53 @@
+//
+//  TimeCollectionVIewCell.swift
+//  VRIC
+//
+//  Created by zeroit01 on 16/12/21.
+//
+
+import UIKit
+
+class TimeCollectionVIewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var LblNext: UILabel!
+    @IBOutlet weak var LblCity: UILabel!
+    @IBOutlet weak var LblTime: UILabel!
+
+}
+
+
+class EventsCollectionVIewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var ImgCover: UIImageView!
+}
+
+
+class TrendingCollectionVIewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var ImgThumbnail: UIImageView!
+    @IBOutlet weak var LblTitle: UILabel!
+    @IBOutlet weak var ImgYoutubeIcon: UIImageView!
+}
+
+class LiveCollectionVIewCell: UICollectionViewCell {
+    @IBOutlet weak var ImgThumbnail: UIImageView!
+    @IBOutlet weak var LblTitle: UILabel!
+}
+
+
+class PodcastCollectionVIewCell: UICollectionViewCell {
+    @IBOutlet weak var ImgThumbnail: UIImageView!
+    @IBOutlet weak var LblTitle: UILabel!
+    @IBOutlet weak var LblDescription: UILabel!
+}
+
+class RamadanCollectionVIewCell: UICollectionViewCell {
+    @IBOutlet weak var ImgThumbnail: UIImageView!
+    @IBOutlet weak var LblTitle: UILabel!
+}
+
+
+class LikeCollectionVIewCell: UICollectionViewCell {
+    @IBOutlet weak var ImgThumbnail: UIImageView!
+    @IBOutlet weak var LblTitle: UILabel!
+}
